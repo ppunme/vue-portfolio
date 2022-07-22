659fb0a3b9c150c57ccf0d3b009a4558623eda0e
ppunme <47684245+ppunme@users.noreply.github.com>
2022-07-23 05:14:03 +1000

    Update responsive design

--- scripts/gh-pages-deploy.js
+++ scripts/gh-pages-deploy.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-console */
-const execa = require("execa");
 const fs = require("fs");
 (async () => {
+  const execa = (await import("execa")).execa;
   try {
     await execa("git", ["checkout", "--orphan", "gh-pages"]);
     // eslint-disable-next-line no-console
@@ -14,7 +14,7 @@ const fs = require("fs");
     console.log("Pushing to gh-pages...");
     await execa("git", ["push", "origin", "HEAD:gh-pages", "--force"]);
     //await execa("del", ["/f/s/q", folderName]);
-    await execa("git", ["checkout", "-f", "master"]);
+    await execa("git", ["checkout", "-f", "main"]);
     await execa("git", ["branch", "-D", "gh-pages"]);
     console.log("Successfully deployed, check your settings");
   } catch (e) {
